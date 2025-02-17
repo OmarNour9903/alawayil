36b666fa391b49a88ad293c0e021fe72adda0a9f
OmarNour9903 <omarnour992003@gmail.com>
2025-02-17 10:38:33 +0200

    second commit

--- main.js
+++ main.js
@@ -19,6 +19,22 @@ document.getElementById('surveyForm').addEventListener('submit', function(event)
     });
 });
 
+document.getElementById('phone').addEventListener('input', function (e) {
+    this.value = this.value.replace(/[^0-9]/g, '');
+});
+
+document.getElementById('name').addEventListener('input', function (e) {
+    this.value = this.value.replace(/[^a-zA-Zأ-ي\s]/g, '');
+});
+
+document.getElementById('university').addEventListener('input', function (e) {
+    this.value = this.value.replace(/[^a-zA-Zأ-ي\s\-\/\\]/g, '');
+});
+
+document.getElementById('subject').addEventListener('input', function (e) {
+    this.value = this.value.replace(/[^a-zA-Zأ-ي\s]/g, '');
+});
+
 document.addEventListener("DOMContentLoaded", function () {
     const link = document.getElementById("trackLink");
 
@@ -32,4 +48,26 @@ document.addEventListener("DOMContentLoaded", function () {
     
     let totalLinkClicks = parseInt(localStorage.getItem("linkClicks")) || 0;
     console.log(`العدد الكلي للضغطات على الرابط: ${totalLinkClicks}`);
+});
+document.getElementById("uploadForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
+    
+    let fileInput = document.getElementById("file");
+    if (fileInput.files.length === 0) {
+        alert("يرجى اختيار ملف.");
+        return;
+    }
+    
+    let formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+
+    let response = await fetch("https://docs.google.com/spreadsheets/d/1tZrcl65xAe5xndrH_YyaVhd9ozr-kLdlf45tnaAoz6o/edit?gid=41449331#gid=41449331", { method: "POST", body: formData });
+
+    let result = await response.json();
+    if (result.status === "success") {
+        document.getElementById("successMessage").style.display = "block";
+        alert("تم الرفع بنجاح! رابط الملف: " + result.link);
+    } else {
+        alert("حدث خطأ: " + result.message);
+    }
 });
