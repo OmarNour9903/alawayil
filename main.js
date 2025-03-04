d814718adfa0d6e413818cff38c99a9eab7529dd
OmarNour9903 <omarnour992003@gmail.com>
2025-03-04 15:50:05 +0200

    second commit

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ document.getElementById('department').addEventListener('input', function (e) {
 
 // form 
 
-let url = "https://script.google.com/macros/s/AKfycbxgD9nZU1x113Lr03iEDPZ1ffxx60sYhJTudNDIz_XrG7dxAJWasKnDzro7Y_vxJkSc/exec"; // 🔹 ضع رابط Google Apps Script هنا
+let url = "https://script.google.com/macros/s/AKfycbxwqXfrheDtXtGp6tGkPSQoSZik7i5SGMujPF_EqWjEbcNXk9x7JFFs7H2NNnXSPDLl/exec"; // 🔹 ضع رابط Google Apps Script هنا
 
 document.getElementById("uploadForm").addEventListener("submit", function (event) {
     event.preventDefault();
@@ -93,11 +93,11 @@ document.getElementById("uploadForm").addEventListener("submit", function (event
 });
 
 window.onload = function() {
-    // تحديد المصدر بناءً على الصفحة أو الشرط المطلوب
-    const sourceParam = new URLSearchParams(window.location.search).get('source');
-    const source = sourceParam === 'internal' ? 'internal' : 'main'; // الافتراضي main
+     // تحديد نوع الزائر بناءً على الصفحة الحالية
+  const isInternal = window.location.href.includes("internal"); // تغيير الشرط حسب احتياجك
+  const source = isInternal ? "internal" : "main";
   
-    fetch(`https://script.google.com/macros/s/AKfycbxgD9nZU1x113Lr03iEDPZ1ffxx60sYhJTudNDIz_XrG7dxAJWasKnDzro7Y_vxJkSc/exec?source=${source}`)
+    fetch(`https://script.google.com/macros/s/AKfycbxwqXfrheDtXtGp6tGkPSQoSZik7i5SGMujPF_EqWjEbcNXk9x7JFFs7H2NNnXSPDLl/exec?source=${source}`)
       .then(response => console.log(`تم تتبع الزائر: ${source}`))
       .catch(error => console.error("خطأ في التتبع:", error));
   };
